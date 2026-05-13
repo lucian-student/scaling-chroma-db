b70b580d4b6507a4891581fa560eda8a432a7e93
Lucián Kučera <kucerl12@fit.cvut.cz>
2026-05-13 13:53:19 +0200

    working on building a image

---
+++ local-infrastructure/control_plane.tf
@@ -0,0 +1,44 @@
+
+
+resource "libvirt_domain" "control_plane" {
+  name   = "example-vm"
+  memory = 4096
+  memory_unit   = "MiB"
+  vcpu   = 2
+  type   = "kvm"
+
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+    boot_devices = ["hd", "network"]
+  }
+
+  devices = {
+    disks = [
+      {
+        source = {
+          file = {
+            file = "/var/lib/libvirt/images/example.qcow2"
+          }
+        }
+        target = {
+          dev = "vda"
+          bus = "virtio"
+        }
+      }
+    ]
+    interfaces = [
+      {
+        model = {
+          type = "virtio"
+        }
+        source = {
+          network = {
+            network = "default"
+          }
+        }
+      }
+    ]
+  }
+}
---
+++ local-infrastructure/image.tf
@@ -0,0 +1,8 @@
+
+resource "libvirt_pool" "chroma_pool" {
+  name = "chroma_pool"
+  type = "dir"
+  target = {
+    path = "/var/lib/libvirt/images/chroma"
+  } 
+}
